ce3ab1a9b91387ef9a6c0f95527aa0f47009fbb9
ZemZem Khiar <zkhiar@outlook.com>
2024-05-16 18:31:05 -0400

    Add initial project files

---
+++ Store/Java_Store.js
@@ -0,0 +1,7 @@
+function greeting(store_name) {
+  console.log(`Thank you for visiting ${store_name}, we hope you have a great time shopping with us!`);
+}
+function app() {
+  greeting("Little Bear's Shop");
+}
+app();
