9cd0f8bdb55438e6cfcaf65f009f195606411b56
ZemZem Khiar <zkhiar@outlook.com>
2024-05-21 14:43:46 -0400

    commit

--- Store/Java_Store.js
+++ Store/Java_Store.js
@@ -1,7 +1,8 @@
 function greeting(store_name) {
-  console.log(`Thank you for visiting ${store_name}, we hope you have a great time shopping with us!`);
+  console.log(`Thank you for visiting ${store_name}, we hope you have a great time shopping with us!`) ;
 }
 function app() {
-  greeting("Little Bear's Shop");
+  for (let i = 0; i < 5; i++)
+  greeting("Hello Kitty's Shop");
 }
 app();
